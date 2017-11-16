7a8c0f40f3a1cab97062c7c0003a60bfa31a2a75
Haseeb Shuaib <shuaib.haseeb@gmail.com>
2017-11-16 17:24:16 -0500

    added pseudocode to each method

--- src/managers/ReportManager.java
+++ src/managers/ReportManager.java
@@ -9,15 +9,16 @@
  *
  */
 public class ReportManager{
-    int numberofJobCreated, currentNumberOfJobs, jobPostingsFilled, jobsNoLongerAvailable,
+    private int numberofJobCreated, currentNumberOfJobs, jobPostingsFilled, jobsNoLongerAvailable,
         numJobListingsStudents, numJobListingsStaff, numJobListingsFaculty;
-    float avgNumApps, avgNumPostFilled, avgNumAppsAvailJobs;
+    private float avgNumApps, avgNumPostFilled, avgNumAppsAvailJobs;
 
-    private ReportManager{
-    this.numJobListingsStaff = getStaffListings();
-    this.numJobListingsFaculty = getFacultyListings();
-    this.numJobListingsStudents = getStudentListings();
-    calculateAverages();
+    private ReportManager(){
+    /*
+     initalize numJobListingsStudents, numJobListingsFaculty, numJobListingsStaff
+     set numberofJobCreated, currentNumberOfJobs, jobPostingsFilled, jobsNoLongerAvailable
+
+     */
     }
 
     /**
@@ -25,30 +26,44 @@ public class ReportManager{
      * to be used for calculations later.
      * @return The number of job listings available for students
      */
-    private int getStudentListings(){return 0;}
+    private int getStudentListings(){
+        /*
+        return JobSystemCoordinator.getStudentListings
+         */
+        return 0;
+    }
     /**
      * Gathers the number of job listings created for students and stores these values in the appropriate fields
      * to be used for calculations later.
      * @return The number of job listings available for Staff
      */
-    private int getStaffListings(){return 0;}
+    private int getStaffListings(){
+        /*
+        return JobSystemCoordinator.getStaffListings
+         */
+        return 0;}
     /**
      * Gathers the number of job listings created for students and stores these values in the appropriate fields
      * to be used for calculations later.
      * @return The number of job listings available for Faculty
      */
-    private int getFacultyListings(){return 0;}
+    private int getFacultyListings(){
+        /*
+        return JobSystemCoordinator.getStaffListings
+         */
+        return 0;}
 
     /**
      * Populates the fields relevant to the job report. Creates calculations based on the information gather by querying
      * other classes.
      */
     private void calculateAverages(){
-        // gather the list of jobs and formulate traverse to fill data
-        // avgNumApps = TOTAL NUMBER OF APPLICANTS FOR ALL JOBS / numOfJobsCreated;
-        // avgNumPostFilled = TOTAL NUMBER OF APPLICANTS FOR POSTFILLED / jobPostingsFilled;
-        // avgNumAppAvailJobs = TOTAL NUMBER OF APPLICANTS FOR AVAIL JOBS / (currentNumberofJobs - (jobPostingsFilled +
-        // jobsNoLongerAvailable)
+
+        /*
+          set avgNumApps to numberofJobCreated / number of total applicants
+          set avgNumPostFilled to total number of applications for post filled jobs / number of applicants for post filled jobs
+          set avgNumAppsAvailJobs to total number of applicants for avail jobs / number of available jobs
+        */
     }
 
     /**
@@ -56,6 +71,8 @@ public class ReportManager{
      *
      * @return The string representations of this object
      */
-    public String toString(){return "";}
+    public String toString(){
+        // build string displaying data in global variables in a readable format
+        return "";}
 
 }
