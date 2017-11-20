55cffbc765baeeb8dd603a6ebc88985c0cde4e50
KevinSilvera <ksilvera@masonlive.gmu.edu>
2017-11-19 23:26:33 -0500

    Removed unnecessary methods in SignUp Manager

--- src/managers/SignUpManager.java
+++ src/managers/SignUpManager.java
@@ -72,31 +72,4 @@ public class SignUpManager {
         System.out.println(success);
         //return applicant;
     }
-
-    /**
-     * Will check to see if there is an Applicant of the same email in the system.
-     * @param email email that will be used to compare
-     * @return a boolean that will indicate if found or not
-     */
-    public boolean checkApplicants(String email){
-        //Will go through the arrayList of applicants
-        //Then compare the emails to see if there is a match
-        //IF there is return true,
-        //otherwise return false
-        return false;
-    }
-
-    /**
-     * Will process the payment for a single applicant then change the paid boolean
-     * @param credit will be the credit card that is given
-     * @param exp will be the expiration date given
-     * @return true if the payment is passed otherwise false
-     */
-    public boolean processPayment(String credit, String exp){
-        //Create a payment instance that will hold the given credit card and expiration date
-        //If a successful payment happens then return true and save payment information in payment
-        //else return false
-        return false;
-    }
-
 }
