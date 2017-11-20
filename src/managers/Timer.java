315875a4f600803ef5e31ae025aaac52c04cc066
pbrown24 <hickoryboy777@gmail.com>
2017-11-20 02:25:55 -0500

    Implemented Timer
    - Implemented the timer class

--- src/managers/Timer.java
+++ src/managers/Timer.java
@@ -1,10 +1,14 @@
 package managers;
 
 import java.time.LocalDate;
+import static coordinators.JobSystemCoordinator.appManager;
+import static coordinators.JobSystemCoordinator.jobManager;
+import entities.Application;
+import entities.Job;
 
 /**
  * Timer will keep track of current date and provide time signals to other manager classes.
- * @author
+ * @author Paul Brown
  */
 public class Timer {
     LocalDate currentDate;
@@ -21,7 +25,7 @@ public class Timer {
      */
     public LocalDate getCurrentDate() {
         // return currentDate
-        return LocalDate.now(); // PLACEHOLDER RETURN
+        return currentDate;
     }
 
     /**
@@ -29,6 +33,7 @@ public class Timer {
      */
     public void dateSignal() {
         // INCREMENT CURRENT DATE BY 1 DAY
+        currentDate.plusDays(1);
     }
 
 
@@ -38,8 +43,22 @@ public class Timer {
     public void monthSignal() {
         // INCREMENT CURRENT MONTH BY 1
         // SET THE DATE TO THE FIRST OF THE MONTH
-        //
-        // REMOVE WITHDRAWN APPLICATION
+        int month,day,year;
+        currentDate.plusMonths(1);
+        month = currentDate.getMonthValue();
+        day = 1;
+        year = currentDate.getYear();
+        currentDate = LocalDate.of(year, month, day);
+        // REMOVE WITHDRAWN APPLICATIONS
+        for(Application application : appManager.applications){
+            if(application.isWithdrawn() == true){
+                appManager.applications.remove(application);
+            }
+        }
+        System.out.println("=================MONTH END SIGNAL=================/n" +
+                           "Withdrawn applications will be cleared from the system/n" +
+                           "at end of each month.");
+        
     }
 
     /**
@@ -49,10 +68,24 @@ public class Timer {
      */
     public void yearSignal() {
         // INCREMENT CURRENT YEAR BY 1
-        // SET THE DATE TO THE FIRST OF THE YEAR
-        //
+        int month,day,year;
+        month = 1;  
+        day = 1;
+        year = currentDate.getYear() + 1;
         // CALL THE MONTH SIGNAL
+        monthSignal();
+        // SET THE DATE TO THE FIRST OF THE YEAR
+        currentDate = LocalDate.of(year, month, day);
+        currentDate.toString();
         // REMOVE CLOSED JOBS AND ALL ASSOCIATIONS
+        for(Job job : jobManager.jobs){
+            if(job.getStatus().equals("CLOSED")){
+                jobManager.jobs.remove(job);
+            }
+        }
+        System.out.println("=================YEAR END SIGNAL=================/n" +
+                           "Closed jobs will be cleared from the system/n" +
+                           "at end of each year.");
     }
 
 } // END CLASS
