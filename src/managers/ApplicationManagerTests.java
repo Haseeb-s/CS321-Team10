49f7111381e39a8116d52e53d1703f323080a7ad
Mathias Wiesbauer <mathias.wiesbauer@gmail.com>
2017-11-30 20:43:54 -0500

    setup testing

---
+++ src/managers/ApplicationManagerTests.java
@@ -0,0 +1,19 @@
+package managers;
+
+import java.io.*;
+import java.util.*;
+import org.junit.*;
+import static org.junit.Assert.*;
+
+
+class ApplicationManagerTests {
+
+
+    @Test
+    public void test() {
+        String str= "junit is working";
+        assertEquals("junit is working", str);
+    } //end test
+
+
+} // END CLASS
