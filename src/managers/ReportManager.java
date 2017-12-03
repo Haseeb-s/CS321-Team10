359299c172a74afed53498bdc6207a9add24de70
Haseeb-s <haseebster@yahoo.com>
2017-12-03 17:49:03 -0500

    Made changes in job & ReportManager to set jobCreationTime to the current time of Timer

--- src/managers/ReportManager.java
+++ src/managers/ReportManager.java
@@ -24,7 +24,7 @@ public class ReportManager{
     private int numberofJobCreated = 0, currentNumberOfJobs = 0, jobPostingsFilled = 0, jobsNoLongerAvailable = 0,
         numJobListingsStudents = 0, numJobListingsStaff = 0, numJobListingsFaculty = 0;
     private double avgNumApps = 0, avgNumPostFilled = 0, avgNumAppsNotAvailJobs = 0;
-
+    private String error = "";
 
     public ReportManager(){
     }
@@ -86,6 +86,14 @@ public class ReportManager{
             }
 
         }
+
+        if (numberofJobCreated == 0){
+            error = "No jobs were found for the desired month and year.\nThe System date is: "
+                    + JobSystemCoordinator.timer.getCurrentDate() + ".\n";
+            if (JobSystemCoordinator.timer.getCurrentDate().toString().equals("2017-01-01") )
+                error = "No jobs were found for the desired month and year.\nThe System date is "
+                        + JobSystemCoordinator.timer.getCurrentDate() + ". (Adjust System date using appropriate signals)\n";
+        }
     }
 
     /**
@@ -95,10 +103,12 @@ public class ReportManager{
      * @return
      */
     public boolean checkDates (String monthYear, String date){
+       System.out.println("The job was created on : " + date);
         String month = monthYear.substring(0, monthYear.indexOf("/"));
         String year = monthYear.substring(monthYear.indexOf("/") + 1, monthYear.length());
         String checkYear = date.toString().substring(2,4);
         String checkMonth = date.toString().substring(5,7);
+      //  System.out.println(month + year + " VS "+ checkMonth +"  " + checkYear);
         if (month.equals(checkMonth) && year.equals(checkYear))
             return true;
         else
@@ -113,13 +123,11 @@ public class ReportManager{
      * @return The string representations of this object
      */
     public String toString(String monthyear){
-
         calculateAverages(monthyear);
-        // build string displaying data in global variables in a readable format
-
+        // build string displaying data in global variables in a readable format;
         return String.format("=================MONTHLY STATISTICS================\n" +
-                "Month : " + JobSystemCoordinator.timer.getCurrentDate().getMonth() + "\n" +
-                "Query type\t\t\t\t\t\t\t\t\tCount\n" +
+                "Month/Year: " + monthyear + "\n" +
+                "Query type                                  Count\n" +
                 "---------------------------------------------------\n" +
                 "Total job created: %28s\n" +
                 "Post filled status: %27s\n" +
@@ -129,9 +137,9 @@ public class ReportManager{
                 "Avg. applicants(no longer available): %11s\n" +
                 "Job listing (faculty): %24s\n" +
                 "Job listing (student): %24s\n" +
-                "Job listing (staff): %26s\n",numberofJobCreated,jobPostingsFilled,
+                "Job listing (staff): %26s\n%s",numberofJobCreated,jobPostingsFilled,
                 jobsNoLongerAvailable,avgNumApps,avgNumPostFilled,avgNumAppsNotAvailJobs, numJobListingsFaculty,numJobListingsStudents,
-                numJobListingsStaff);
+                numJobListingsStaff, error);
     }
 
 }
